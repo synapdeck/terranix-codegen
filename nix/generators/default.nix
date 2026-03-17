569cc64cdcbc9d3f208a52a72a3f0e197cb49811
Alexis Williams <alexis@typedr.at>
2026-03-17 15:59:10 -0700

    feat(nix): add generateProvider with tofu and plugin derivation paths

--- nix/generators/default.nix
+++ nix/generators/default.nix
@@ -17,5 +17,35 @@
       '';
     };
 
-  generateProvider = throw "not yet implemented";
+  generateProvider = {
+    provider,
+    pluginDrv ? null,
+    terraform ? null,
+    tofu ? pkgs.opentofu,
+    outputDir ? "providers",
+  }: let
+    usePlugin = pluginDrv != null;
+    terraformWithPlugin = assert pkgs.lib.assertMsg (terraform != null)
+    "generateProvider: terraform must be provided when pluginDrv is set";
+      terraform.withPlugins (_: [pluginDrv]);
+    tfExecutable =
+      if usePlugin
+      then terraformWithPlugin
+      else tofu;
+    tfName =
+      if usePlugin
+      then "terraform"
+      else builtins.baseNameOf (pkgs.lib.getExe tfExecutable);
+  in
+    pkgs.stdenv.mkDerivation {
+      name = "terranix-generated-modules";
+      dontUnpack = true;
+      dontInstall = true;
+      nativeBuildInputs = [terranix-codegen tfExecutable];
+      buildPhase = ''
+        export HOME=$(mktemp -d)
+        mkdir -p $out/${outputDir}
+        terranix-codegen generate -p ${provider} -t ${tfName} -o $out/${outputDir}
+      '';
+    };
 }
